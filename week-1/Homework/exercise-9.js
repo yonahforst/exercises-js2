5f2ffb66173211183d59436e731c6be7e44110e7
Yonah Forst <yonaforst@hotmail.com>
2020-09-08 12:22:51 +0200

    completed lesson 1, homework, exercise 9

--- week-1/Homework/exercise-9.js
+++ week-1/Homework/exercise-9.js
@@ -41,25 +41,123 @@ var product2 = {
   price: 9.99,
   stock: 2
 };
+var product3 = {
+  id: 3,
+  name: "Computer mouse",
+  price: 20.50,
+  stock: 10,
+}
+var product4 = {
+  id: 4,
+  name: "Coffee mug",
+  price: 2.99,
+  stock: 78,
+}
 
 products.push(product1);
 products.push(product2);
+products.push(product3);
+products.push(product4);
 
 var shoppingCart = {
   totalPrice: 0,
   selectedProducts: []
 };
-
+/**
+when i call addToShoppingCart(4)
+shopping cart should equal: {
+  totalPrice: 2.99,
+  selectedProducts: [ product4 ]
+}
+then when i call addToShoppingCard(2)
+shopping cart should equal: {
+  totalPrice: 12.98,
+  selectProducts: [ product4, product2 ]
+}
+*/
 function addToShoppingCart(id){
+  //find product in products array which has the given ID (if none, return)
+  var product = products.find(product => product.id === id)
+  if (!product || product.stock < 1)
+    return
 
+  //push product to shopping cart's selected product array
+  shoppingCart.selectedProducts.push(product)
+  //add product price to existing total price of the shopping cart
+  shoppingCart.totalPrice = shoppingCart.totalPrice + product.price
 }
 
+/**
+ if shopping cart equals: {
+  totalPrice: 12.98,
+  selectedProducts: [ product4, product2 ]
+}
+and i call removeFromShoppingCart(999)
+nothing should happen
+if i call removeFromShoppingCart(4)
+the shopping cart should equal: {
+  totalPrice: 9.99,
+  selectedProducts: [ product2 ]
+}
+*/
 function removeFromShoppingCart(id){
+  // find product in shopping cart. If it doesn't exist, return.
+  var productIndex = shoppingCart.selectedProducts.findIndex(product => product.id === id)
+  if (productIndex === -1)
+    return
 
+
+  // remove product from shopping cart
+  var removedProducts = shoppingCart.selectedProducts.splice(productIndex, 1)
+  
+  var product = removedProducts[0]
+
+  // subtract the product price from the shopping carts total price
+  shoppingCart.totalPrice = shoppingCart.totalPrice - product.price
 }
 
-function shop(){
+//// ANOTHER WAY OF WRITING THE FUNCTION
+// function removeFromShoppingCart(id){
+//   // find product in shopping cart. If it doesn't exist, return.
+//   var product = shoppingCart.selectedProducts.find(product => product.id === id)
+//   if (!product)
+//     return
 
+//   // remove product from shopping cart
+//   shoppingCart.selectedProducts = shoppingCart.selectedProducts.filter(product => product.id !== id)
+
+//   // subtract the product price from the shopping carts total price
+//   shoppingCart.totalPrice = shoppingCart.totalPrice - product.price
+// }
+
+
+
+/**
+ if shopping cart equals: {
+  totalPrice: 12.98,
+  selectedProducts: [ product4, product2 ]
+}
+and i call shop(), the shoppingCart should equal: {
+  totalPrice: 0,
+  selectedProducts: []
+}
+and product4.stock should be one less
+and product2.stock should be one less
+*/
+function shop(){
+  // for each product in the selected products:
+  // find the product in our available products array
+  // and remove 1 from the inventory
+  for (const selectedProduct of shoppingCart.selectedProducts) {
+    const product = products.find(product => product.id === selectedProduct.id)
+    product.stock--
+  }
+
+  // after, set the total price to 0 and the selected products to []
+  shoppingCart = {
+    selectedProducts: [],
+    totalPrice: 0
+  }
 }
 
 //results
