7367743171e30509b66920196f532c18427b20ff
Yonah Forst <yonaforst@hotmail.com>
2020-09-12 11:49:40 +0200

    finished week2 homework exercise1

--- week-2/Homework/1-exercises/exercises.js
+++ week-2/Homework/1-exercises/exercises.js
@@ -13,10 +13,26 @@
  *      .....
  * </div>
  */
+
 function exerciseOne(arrayOfPeople) {
   let content = document.querySelector("#content");
+  // for each person in the array:
+  arrayOfPeople.forEach(person => {
+    //   create a new h1 element
+    var h1 = document.createElement('h1')
+    //   set the inner text to name
+    h1.innerText = person.name
+    //   create a new h2 element
+    var h2 = document.createElement('h2')
+    //   set the inner text to job
+    h2.innerText = person.job
+    //   content.appendChild the h1
+    content.appendChild(h1)
+    //   content.appendChild the h2
+    content.appendChild(h2)
+  })
 }
-
+  
 /**
  *
  * Create a list of shopping items. You should use an unordered list.
@@ -25,7 +41,21 @@ function exerciseOne(arrayOfPeople) {
  *
  */
 function exerciseTwo(shopping) {
-  //Write your code in here
+  // find the div with id #content
+  let content = document.querySelector('#content')
+  // create a new ul element
+  let ul = document.createElement('ul')
+  // for each element in the shopping list:
+  shopping.forEach(itemName => {
+    //   create a new li element
+    let li = document.createElement('li')
+    //   set the inner text to the shopping list element
+    li.innerText = itemName
+    //   appendChild the li element to the ul element
+    ul.appendChild(li)
+  })
+  // appendChild the ul elment to the content element
+  content.appendChild(ul)
 }
 
 /**
@@ -59,6 +89,39 @@ function exerciseTwo(shopping) {
 **/
 function exerciseThree(books) {
   //Write your code in here
+  // find the div with id content
+  let content = document.querySelector('#content')
+  // create the ul element
+  let ul = document.createElement('ul')
+  // for each element in the books array:
+  books.forEach(book => {
+    //   create an li element
+    let li = document.createElement('li')
+    //   create a p element
+    let p = document.createElement('p')
+    //   set the inner text of the p elment to the title + author
+    p.innerText = book.title + ' - ' + book.author
+    //   create a img elment
+    let img = document.createElement('img')
+    //   set the src property to the img url (where do we get this from??)
+    img.src = book.imgUrl
+    //   if alreadyRead is true
+    if (book.alreadyRead) {
+    //     set the background color of the p element to green
+      li.style.backgroundColor = 'green'
+    } else { //   else
+      //     set the background color of the p element to red
+      li.style.backgroundColor = 'red'
+    }
+    //   append the p element as child of the li element
+    li.appendChild(p)
+    //   append the img element as a child of the li element
+    li.appendChild(img)
+    //   append the li element as a child of the ul element
+    ul.appendChild(li)
+  })
+  // append the ul element as a child of the content div
+  content.appendChild(ul)
 }
 
 //
@@ -87,17 +150,20 @@ const books = [
   {
     title: "The Design of Everyday Things",
     author: "Don Norman",
-    alreadyRead: false
+    alreadyRead: false,
+    imgUrl: './images/design.jpeg'
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
-    alreadyRead: true
+    alreadyRead: true,
+    imgUrl: './images/human.jpeg'
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
-    alreadyRead: true
+    alreadyRead: true,
+    imgUrl: './images/pragmatic.jpeg'
   }
 ];
 
